0fe6673bbd087789f884ddb41da073b9215f9578
sivarobo <67424996+sivarobo@users.noreply.github.com>
2026-06-05 08:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from google import genai
+import google.generativeai as genai
 from google.genai import types
 from PIL import Image
 from docx import Document
